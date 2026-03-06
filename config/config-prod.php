6683abf812a1fd3e61391558b7aebb73e3edf27e
shivapathem <shiva.pathem.ext@bbc.co.uk>
2026-03-06 19:48:43 +0530

    Update config-prod.php

--- config/config-prod.php
+++ config/config-prod.php
@@ -1,7 +1,5 @@
 <?php
-return [
- 'environment' => 'production',
- 'db_host' => 'prod-db-server',
- 'db_user' => 'produser',
- 'db_pass' => 'prodpassword'
-];
+$db_host="prod-db-server";
+$db_user="prod-user";
+$db_password="prod-password";
+?>
